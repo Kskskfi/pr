b87678f17e47b0ede365e92368cfcaaeac8ad0dc
Kskskfi <ogyrhik321@gmail.com>
2025-05-14 08:08:09 +0700

    first commit

---
+++ app/Controller/Site.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Controller;
+
+
+use Src\View;
+
+class Site
+{
+    public function index(): string
+    {
+        $posts = Post::all();
+        return (new View())->render('site.post', ['posts' => $posts]);
+    }
+
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
+    }
+}
