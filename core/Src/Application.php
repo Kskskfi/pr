34176b186a6c8b500eb8b11a9b05e2d35e686ef1
Kskskfi <ogyrhik321@gmail.com>
2025-05-14 11:11:44 +0700

    composer

--- core/Src/Application.php
+++ core/Src/Application.php
@@ -1,15 +1,20 @@
 <?php
+
 namespace Src;
+
 use Error;
+
 class Application
 {
     private Settings $settings;
     private Route $route;
+
     public function __construct(Settings $settings)
     {
         $this->settings = $settings;
         $this->route = new Route();
     }
+
     public function __get($key)
     {
         if ($key === 'settings') {
@@ -17,6 +22,7 @@ class Application
         }
         throw new Error('Accessing a non-existent property');
     }
+
     public function run(): void
     {
         $this->route->setPrefix($this->settings->getRootPath());
--- core/Src/Settings.php
+++ core/Src/Settings.php
@@ -1,13 +1,18 @@
 <?php
+
 namespace Src;
+
 use Error;
+
 class Settings
 {
     private array $_settings;
+
     public function __construct(array $settings = [])
     {
         $this->_settings = $settings;
     }
+
     public function __get($key)
     {
         if (array_key_exists($key, $this->_settings)) {
@@ -15,10 +20,12 @@ class Settings
         }
         throw new Error('Accessing a non-existent property');
     }
+
     public function getRootPath(): string
     {
         return $this->path['root'] ? '/' . $this->path['root'] : '';
     }
+
     public function getViewsPath(): string
     {
         return '/' . $this->path['views'] ?? '';
